b249aa5e8d7c66dfe648dd76d3d8fa1ffd5347cb
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-08-30 15:30:15 -0700

    Database config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
 // const conString = 'postgres://postgres:Tableofcont1@localhost:5432/kilovolt';
-const conString = 'postgres://postgres:password@localhost:5432/kilovolt'; // DNE: Don't forget to set your own conString
+const conString = `${process.env.DATABASE_URL}`; // DNE: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
